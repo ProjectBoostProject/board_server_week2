f25ab3095feb74a65585403135f809b80405a2ef
Dongwook9696 <59466899+Dongwook9696@users.noreply.github.com>
2021-10-14 14:49:33 +0900

    2주차 스키마

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,25 @@
 DROP TABLE IF EXISTS Board;
 DROP TABLE IF EXISTS Reply;
+DROP TABLE IF EXISTS Menu;
 
+CREATE TABLE Menu
+(
+    id          INT     PRIMARY KEY AUTO_INCREMENT,
+    board_name  VARCHAR(50),
+    introduce   VARCHAR(255);
+)
 CREATE TABLE Board
 (
     id      INT          PRIMARY KEY AUTO_INCREMENT,
+    menu_id INT,
     title   VARCHAR(255) NOT NULL,
     writer  VARCHAR(20)  NOT NULL,
     content VARCHAR(999) NOT NULL,
     password VARCHAR(20) NOT NULL,
     create_date TIMESTAMP  DEFAULT NOW(),
-    update_date TIMESTAMP  DEFAULT NOW()
+    update_date TIMESTAMP  DEFAULT NOW(),
+    FOREIGN KEY (menu_id)
+    REFERENCES Menu(id) On DELETE CASCADE
 );
 
 CREATE TABLE Reply
